1a7b6a63f54c79a1acc15873ab305f2d4162a24c
Daniel Macias <dmacias@artemis.cs.csub.edu>
2021-12-07 08:24:28 -0800

    fix password check

--- php/loginEmployee.php
+++ php/loginEmployee.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-<title> Employee account registration</title>
+<title>Eposting </title>
 </head>
 <body>
 	<input type="button" onclick="location.href='index.php';" value="Back"/>
@@ -40,9 +40,7 @@ if (isset($_POST['Login'])) {
 	}
 	$validation->bind_param('s',$username);
 	if ($validation->execute()) {
-		if (mysqli_stmt_bind_result($validation, $res_id,$res_fname,$res_lname,
-			$res_DOB,$res_SSN,$res_address,$res_city,$res_state,$res_email,$res_user,
-			$res_password)) {
+		if (mysqli_stmt_bind_result($validation, $res_id,$res_fname,$res_lname,$res_DOB,$res_SSN,$res_address,$res_city,$res_state,$res_email,$res_user,$res_password)) {
 
 			$result_count = 0;
 
@@ -51,33 +49,42 @@ if (isset($_POST['Login'])) {
 			}
 
 			if ($result_count == 0) {
-				$_SESSION["error"] = "Error: username and/or password combination was not found ";
+				$_SESSION["error"] = "Error1: username and/or password combination was not found ";
 				header("Location: loginEmployee.php");
 			}
 			else {
-				//for hash and not hash passwords
-				//the password has to be hash in registerEmployee
-//				$isGood = $Mariadb->prepare("SELECT * FROM employee WHERE password=?");
-//				if (!$isGood) {
-//					echo "Error getting result: password error " , mysqli_error($Mariadb);
-//					die();
-//				}
-//				$isGood->bind_param('s',$password);
 
-				$isGood = password_verify($password, $res_password);
+
+				$isGood = password_verify($password,$res_password );
 
 				if ($isGood) {
 					$_SESSION["UserID"] = $res_id;
 					$_SESSION["username"] = $res_user;
 					$_SESSION["Fname"] = $res_fname;
 					$_SESSION["Lname"] = $res_lname;
-					$_SESSION["password"] = $password;
+					$_SESSION["param_password"] = $password;
+					$_SESSION["password"] = $res_password;
 
 					header("Location: Employee.php");
 				}
 				else {
-					$_SESSION["error"] = "Error: the username and/or password combination was not found";
-					header("Location: loginEmployee.php");
+					$hash = password_hash($res_password, PASSWORD_DEFAULT);
+					$isGood = password_verify($password, $hash);
+
+					if ($isGood) {
+						$_SESSION["UserID"] = $res_id;
+						$_SESSION["username"] = $res_user;
+						$_SESSION["Fname"] = $res_fname;
+						$_SESSION["Lname"] = $res_lname;
+						$_SESSION["param_password"] = $password;
+						$_SESSION["password"] = $res_password;
+
+						header("Location: Employee.php");
+
+					} else {
+						$_SESSION["error"] = "Error2: the username and/or password combination was not found";
+						header("Location: loginEmployee.php");
+					}
 				}
 			}
 
